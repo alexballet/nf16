beb3188733da7230386cfbc3f15fb21ed707cf63
alexballet <brasilialex64@hotmail.fr>
2015-10-16 11:12:01 +0200

    Create tp3.h

---
+++ tp3.h
@@ -0,0 +1,46 @@
+#ifndef tp3_h
+#define tp3_h
+
+
+#endif /* tp3_h */
+
+
+//Structures
+
+struct Livre
+{
+    char titre[50];
+    char auteur[50];
+    char edition[50];
+    int disponible;
+    struct Livre* suivant;
+};
+
+typedef struct Livre T_Livre;
+
+struct Rayon
+{
+    char theme_rayon[50];
+    int nombre_livres;
+    T_Livre* premier;
+    struct Rayon* suivant;
+};
+
+typedef struct Rayon T_Rayon;
+
+struct Biblio
+{
+    char nom[50];
+    struct Rayon* premier;
+};
+
+typedef struct Biblio T_Biblio;
+
+//prototypes
+
+T_Livre *creerLivre (char *titre, char *auteur, char *edition);
+T_Biblio *creerBiblio (char *nom);
+T_Rayon *creerRayon(char *theme);
+int ajouterLivre(T_Rayon *rayon, T_Livre *livre);
+int emprunterLivre(T_Rayon *rayon, char* titre);
+

bdee019c8d7b911b08a2f5105cec77fcd3b1de86
alexballet <brasilialex64@hotmail.fr>
2015-11-04 18:47:56 +0100

    Update tp3.c

--- tp3.c
+++ tp3.c
@@ -4,15 +4,24 @@
 
 #include "tp3.h"
 
-//Fonctions de crÔøΩation Livre, Rayon, Biblio
+/*
+ **********************************************************
+ DEBUT DES FONCTIONS UTILITAIRE POUR GENERER DES STRUCTURES
+ **********************************************************
+ */
 
-T_Livre *creerLivre (char *titre, char *auteur, char *edition)
+
+//Fonction de création d'un livre
+T_Livre* creerLivre (char *titre, char *auteur, char *edition)
 {
     T_Livre *new_livre = (T_Livre*)malloc(sizeof(T_Livre));
+    //Revoir l'allocation dynamique comme la sugestion faite à la fonction creerRayon
     
+    //Test si l'allocation s'est bien déroulée
     if(new_livre==NULL)
         exit(EXIT_FAILURE);
     
+    //Initialisation de la structure
     strcpy(new_livre->titre,titre);
     strcpy(new_livre->auteur,auteur);
     strcpy(new_livre->edition,edition);
@@ -22,35 +31,74 @@ T_Livre *creerLivre (char *titre, char *auteur, char *edition)
     return new_livre;
 }
 
-T_Rayon *creerRayon(char *theme)
+
+
+//Fonction de création d'un rayon
+T_Rayon* creerRayon(char *theme)
 {
     T_Rayon *new_rayon = (T_Rayon*)malloc(sizeof(T_Rayon));
+    /*
+     pourquoi tu ne fais pas :
+     -->    T_Rayon *new_rayon = malloc(sizeof(*new_rayon));
+     ????????
+     */
     
+    //Test si l'allocation s'est bien déroulé
     if(new_rayon==NULL)
         exit(EXIT_FAILURE);
     
+    //Initialisation de la structure
     strcpy(new_rayon->theme_rayon,theme);
     new_rayon->nombre_livres=0;
+    new_rayon->suivant=NULL;
     new_rayon->premier=NULL;
     
     return new_rayon;
 }
 
-T_Biblio *creerBiblio (char *nom)
+
+
+//Fontion de création de la bibliothèque
+T_Biblio* creerBiblio (char *nom)
 {
     T_Biblio *new_biblio = (T_Biblio*)malloc(sizeof(T_Biblio));
     
+    //Test si l'allocation s'est bien déroulée
     if(new_biblio==NULL)
         exit(EXIT_FAILURE);
     
+    //Initialisation de la structure
     strcpy(new_biblio->nom,nom);
     new_biblio->premier=NULL;
     
     return new_biblio;
 }
 
-//fonction d'ajout d'un livre dans un rayon
 
+/*
+ ********************************************
+ FIN DES FONCTIONS DE CREATION DES STRUCTURES
+ ********************************************
+ */
+
+
+
+
+
+
+
+
+
+
+
+/*
+ ******************************
+ DEBUT DES FONCTIONS DE LINKAGE
+ ******************************
+ */
+
+
+//fonction d'ajout d'un livre dans un rayon
 int ajouterLivre(T_Rayon *rayon, T_Livre *livre)
 {
     T_Livre *ptr_livre = rayon->premier;
@@ -81,8 +129,129 @@ int ajouterLivre(T_Rayon *rayon, T_Livre *livre)
     return 0;
 }
 
-//fonction emprunter livre dans un rayon
 
+/*
+ On souhaite ajouter le rayon à la bibliothèque
+ Pour cela on suppose que le rayon est déjà créé. Le conflit se gère directement dans le main
+ Notre but est d'insérer le rayon dans la bibliothèque
+ */
+int ajouterRayon(T_Biblio *biblio, T_Rayon *rayon)
+{
+    T_Rayon *ptr_rayon = biblio->premier;
+    
+    if(!biblio->premier)
+    {
+        biblio->premier=rayon;
+        rayon->suivant=NULL;
+        ptr_rayon=rayon;
+    }
+    else
+    {
+        while(ptr_rayon->suivant && strcmp(rayon->theme_rayon,ptr_rayon->suivant->theme_rayon)>0)
+            ptr_rayon=ptr_rayon->suivant;
+        
+        if(!ptr_rayon->suivant || strcmp(rayon->theme_rayon, ptr_rayon->suivant->theme_rayon)!=0)
+        {                                                       //vérifier que le livre n'existe pas déjà dans le rayon !
+            rayon->suivant=ptr_rayon->suivant;
+            ptr_rayon->suivant=rayon;
+            ptr_rayon=rayon;
+        }
+    }
+    
+    if(strcmp(ptr_rayon->theme_rayon,rayon->theme_rayon)==0)
+        return 1;
+    
+    return 0;
+}
+
+
+/*
+ ****************************
+ FIN DES FONCTIONS DE LINKAGE
+ ****************************
+ */
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *******************************
+ DEBUT DES FONCTIONS D'AFFICHAGE
+ *******************************
+ */
+
+
+//Fonction d'affichage des rayons de la bibliothèque
+void afficherBiblio(T_Biblio *biblio)
+{
+    //Creation d'un pointeur afin de manipuler la liste chainee
+    T_Rayon *ptr_nb_livres = biblio->premier;
+    
+    //On parcout la liste
+    //La liste étant déjà trié, on se contente d'afficher simplement dans l'odre ou on la parcourt
+    while (ptr_nb_livres)
+    {
+        printf("Le Rayon : %s contient %d livres\n",ptr_nb_livres->theme_rayon,ptr_nb_livres->nombre_livres);
+        ptr_nb_livres=ptr_nb_livres->suivant;
+    }
+}
+
+
+//Fonction d'affichage du contenu d'un rayon
+void afficherRayon(T_Rayon *rayon)
+{
+    //Pointeur sur la structure des livres. Récupère la première adresse de la sous liste chainée
+    T_Livre *ptr_rayon = rayon->premier;
+    
+    while(ptr_rayon->suivant)
+    {
+        //Si livre Disponible
+        if (ptr_rayon->disponible==1)
+        {
+            printf("Titre : %s\tAuteur : %s\tEdition : %s\tDisponibilité : Oui\n",ptr_rayon->titre,ptr_rayon->auteur,ptr_rayon->edition);
+        }
+        //Si livre non Disponible
+        else
+        {
+            printf("Titre : %s\tAuteur : %s\tEdition : %s\tDisponibilité : Non\n",ptr_rayon->titre,ptr_rayon->auteur,ptr_rayon->edition);
+        }
+        //incrémentation du pointeur
+        ptr_rayon=ptr_rayon->suivant;
+    }
+    
+}
+
+
+/*
+ *****************************
+ FIN DES FONCTIONS D'AFFICHAGE
+ *****************************
+ */
+
+
+
+
+
+
+
+
+
+
+/*
+ **********************************************
+ DEBUT DES FONCTIONS DE SUPPRESSION & D'EMPRUNT
+ **********************************************
+ */
+
+
+//fonction emprunter livre dans un rayon
 int emprunterLivre(T_Rayon *rayon, char* titre)
 {
     T_Livre *ptr = rayon->premier;
@@ -103,6 +272,8 @@ int emprunterLivre(T_Rayon *rayon, char* titre)
     return 0;
 }
 
+
+
 //fonction suppression livre rayon
 int supprimerLivre(T_Rayon *rayon, char* titre)
 {
@@ -121,7 +292,7 @@ int supprimerLivre(T_Rayon *rayon, char* titre)
     {
         while (ptr->suivant && (strcmp(ptr->suivant->titre,titre)!=0))
             ptr=ptr->suivant;
-
+        
         if(!ptr->suivant)
             printf("Le livre n'exite pas dans ce rayon\n");
         else
@@ -135,6 +306,37 @@ int supprimerLivre(T_Rayon *rayon, char* titre)
     }
     return 0;
 }
+
+
+/*
+ Suppression d'un rayon et de tous les livres qu'il contient
+ renvoie 1 si la suppression s'est bien passée, 0 sinon; on veillera à libérer la mémoire précédemment allouée
+ */
+void supprimerRayon(T_Biblio *biblio, char *nom_rayon)
+{
+    
+}
+
+
+/*
+ ********************************************
+ FIN DES FONCTIONS DE SUPPRESSION & D'EMPRUNT
+ ********************************************
+ */
+
+
+
+
+
+
+
+
+/*
+ ***********************************
+ DEBUT DES FONCTIONS COMPLÉMENTAIRES
+ ***********************************
+ */
+
 
 //fonction de tri de tableau
 void trierTableau(char ***tab, int tailleTab)
@@ -163,6 +365,7 @@ void trierTableau(char ***tab, int tailleTab)
     
 }
 
+
 //fonction de recherche de livres
 void rechercherLivres(T_Biblio *biblio, char* critereTitre)
 {
@@ -170,7 +373,7 @@ void rechercherLivres(T_Biblio *biblio, char* critereTitre)
     int nbLivres=0, i,j;
     
     char ***tableau;// création tableau des livres trouvés
-    tableau=malloc(sizeof(char**)*50);
+    tableau=malloc(sizeof(char**)*tailleTabLivres);
     
     for (i=0; i<50; i++)
     {
@@ -214,67 +417,11 @@ void rechercherLivres(T_Biblio *biblio, char* critereTitre)
         }
         printf("\n");
     }
-
+    
 }
 
-int main()
-{
-    
-    T_Biblio *UTC = creerBiblio("UTC");
-    T_Rayon *Maths = creerRayon("Maths");
-    T_Rayon *Biologie = creerRayon("Biologie");
-    T_Livre *Manuel_MT90 = creerLivre("Manuel MT90", "Prof", "2009");
-    T_Livre *Manuel_MT91 = creerLivre("Manuel MT91", "Prof", "2010");
-    T_Livre *Manuel_BL01 = creerLivre("Manuel BL01", "Prof2", "2013");
-    
-    char titre[50];
-
-    
-    if (ajouterLivre(Maths, Manuel_MT90)!=0)
-        printf("Le premier livre du rayon %s de la bibliotheque %s est %s\n", Maths->theme_rayon, UTC->nom, Maths->premier->titre);
-    else
-        printf("Erreur lors de l'ajout du livre %s\n", Manuel_MT90->titre);
-    
-    if (ajouterLivre(Maths, Manuel_MT91)!=0)
-        printf("Le second livre du rayon %s de la bibliotheque %s est %s\n", Maths->theme_rayon, UTC->nom, Maths->premier->suivant->titre);
-    else
-        printf("Erreur lors de l'ajout du livre %s\n", Manuel_MT91->titre);
-    
-    if (ajouterLivre(Biologie, Manuel_BL01)!=0)
-        printf("Le premier livre du rayon %s de la bibliotheque %s est %s\n", Biologie->theme_rayon, UTC->nom, Biologie->premier->titre);
-    else
-        printf("Erreur lors de l'ajout du livre %s\n", Manuel_BL01->titre);
-    
-    
-    
-    
-    T_Livre *ptr = Maths->premier; // Affichage livres rayon "Maths" après ajout
-    
-    while(ptr)
-    {
-        printf("%s\n", ptr->titre);
-        ptr = ptr->suivant;
-    }
-    
-    rechercherLivres(UTC, "Manuel");// test fonction recherche de livres
-    
-    printf("Entrez le nom du livre à emprunter dans le rayon Maths : "); // test de la fonction emprunter livre
-    scanf("%[^\n]s", titre);
-    getchar();
-    
-    emprunterLivre(Maths, titre);
-    
-    printf("Entrez le nom du livre à emprunter dans le rayon Maths : ");
-    scanf("%[^\n]s", titre);
-    getchar();
-    
-    emprunterLivre(Maths, titre);
-    
-    printf("Entrez le nom du livre à supprimer dans le rayon Maths : ");// test de la fonction supprimer livre
-    scanf("%[^\n]s", titre);
-    getchar();
-    
-    supprimerLivre(Maths, titre);
-    
-    return 0;
-}
+/*
+ *********************************
+ FIN DES FONCTIONS COMPLÉMENTAIRES
+ *********************************
+ */
